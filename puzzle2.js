47bf65734197907c5c06d27455ef8cb21dd47a0f
clariti23 <g.ferowich@gmail.com>
2020-11-19 16:53:06 -0600

    puzzle 2 and 1

---
+++ puzzle2.js
@@ -0,0 +1,50 @@
+
+
+https://adventofcode.com/2015/day/2/input
+
+
+// takes in "2x3x4"
+function calcSA(input) {
+
+    let subtotal = 0;
+    let sides = input.split("x");
+
+    //add smallest area as extra
+    let mins = sides.sort((a, b) => a - b).slice(0, 2);
+    let smallest = mins[0] * mins[1];
+    subtotal += smallest;
+
+    let l = sides[0];
+    let w = sides[1];
+    let h = sides[2];
+
+    value = 2 * (l * w) + 2 * (l * h) + 2 * (h * w);
+    subtotal += value;
+
+    // console.log(subtotal)
+    return subtotal;
+}
+
+// let input = "2x3x4";
+// calcSA(input)
+
+
+const calcTotal = () =>{
+    
+    let total = 0;
+
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('input2.txt')
+      });
+      
+    
+    lineReader.on('line', function (line) {
+        // console.log('Line from file:', line);
+        let x = calcSA(line);
+        total += x
+        console.log(total)
+    });
+    
+}
+
+calcTotal();
