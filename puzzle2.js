80b3297e2e0b222440de2d4f49c8dc14c763e1fe
clariti23 <g.ferowich@gmail.com>
2020-11-19 20:47:47 -0600

    puzzle 2

--- puzzle2.js
+++ puzzle2.js
@@ -29,22 +29,55 @@ function calcSA(input) {
 // calcSA(input)
 
 
-const calcTotal = () =>{
+const calcTotal = () => {
     
     let total = 0;
 
     var lineReader = require('readline').createInterface({
         input: require('fs').createReadStream('input2.txt')
-      });
+    });
       
-    
     lineReader.on('line', function (line) {
-        // console.log('Line from file:', line);
         let x = calcSA(line);
         total += x
         console.log(total)
     });
-    
 }
 
-calcTotal();
+// calcTotal();
+
+// accepts "2x3x4"
+function calculatePerimeter(input) {
+    // let subtotal = 0;
+    let sides = input.split("x");
+
+    //assign the two smallest sides
+    let mins = sides.sort((a, b) => a - b).slice(0, 3);
+    let small1 = mins[0];
+    let small2 = mins[1];
+    
+    let ribbon = 2 * small1 + 2 * small2 + (mins[0] * mins[1] * mins[2])
+
+    // console.log(ribbon)
+
+    return ribbon;
+}
+
+// calculatePerimeter("2x3x4");
+
+const calcPerimTotal = () => {
+    
+    let total = 0;
+
+    var lineReader = require('readline').createInterface({
+        input: require('fs').createReadStream('input2.txt')
+    });
+      
+    lineReader.on('line', function (line) {
+        let x = calculatePerimeter(line);
+        total += x
+        console.log(total)
+    });
+}
+
+calcPerimTotal();
